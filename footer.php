6b9ddbcd5387504ac19ae6bf15d5753b7ca1cbf2
Fabrício Augusto <fabricio868@hotmail.com>
2014-05-20 15:09:42 -0300

    Desenvolvendo o Rodapé

--- footer.php
+++ footer.php
@@ -1,8 +1,18 @@
 	</div>
 	<!--- - - - - - - - - - - - - - - - - - - - - - - - Rodapé - - - - - - - - - - - - - - - - - - - - - - - -->
 	<div id="rodape">
-		<div id="conteudo_rodape">
-
+		<div class="conteudo_rodape">
+			<div class="catalogo"><div style="float:left"><img src="<?php echo get_template_directory_uri(); ?>/img/seta.png" /></div>
+				<div style="float:left; width:160px; margin-left:23px"><span>Catalogo em pdf</span></br>
+				<p>Baixe nosso Catálogo de produto em PDF</p></div>
+			</div>
+			<div class="contatos">
+				<div class="local left"><img src="<?php echo get_template_directory_uri(); ?>/img/local.png" /></div>
+				<div class="tel left"><img src="<?php echo get_template_directory_uri(); ?>/img/tel.png" /></div>
+				<div class="email left"><img src="<?php echo get_template_directory_uri(); ?>/img/email.png" /></div>
+				<div class="facebook left"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" /></div>
+			</div>
+			<div class="copyright"></div>
 		</div>
 	</div>
 <?php wp_footer(); ?>
